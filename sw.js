8939581c7811f5cde4bde27725b0f958b4012350
TuckCos <tuck.cosley@outlook.com>
2026-09-06 16:39:25 -0500

    Upgrade service worker cache version to v3

--- sw.js
+++ sw.js
@@ -1,34 +1,41 @@
-const CACHE='centrol-yield-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./centrol-logo.png','./icon-192.png','./icon-512.png'];
+const CACHE='centrol-yield-v3';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './centrol-logo.png',
+  './icon-192.png',
+  './icon-512.png'
+];
 
-self.addEventListener('install',e=>
+self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE)
-      .then(c=>c.addAll(ASSETS))
-      .then(()=>self.skipWaiting())
-  )
-);
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
 
-self.addEventListener('activate',e=>
+self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
-      .then(keys=>Promise.all(
-        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
       ))
-      .then(()=>self.clients.claim())
-  )
-);
+      .then(() => self.clients.claim())
+  );
+});
 
-self.addEventListener('fetch',e=>
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r=>
+    caches.match(e.request).then(r =>
       r || fetch(e.request)
-        .then(resp=>{
-          const copy=resp.clone();
-          caches.open(CACHE).then(c=>c.put(e.request,copy));
+        .then(resp => {
+          const copy = resp.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
           return resp;
         })
-        .catch(()=>caches.match('./index.html'))
+        .catch(() => caches.match('./index.html'))
     )
-  )
-);
+  );
+});
